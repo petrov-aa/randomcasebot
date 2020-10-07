68a76076f9b73b295d374e5c78568d3a2e73c5e1
Aleksandr Petrov <petrov.aa@phystech.edu>
2020-10-07 17:57:14 +0300

    fix bot

--- bot.py
+++ bot.py
@@ -1,6 +1,7 @@
 import os
 import random
 import re
+import hashlib
 
 import telebot
 from telebot import types
@@ -22,28 +23,31 @@ def random_style_letter(letter):
   try_filters = list(filter(lambda f: f is not prev_filter, filters))
   prev_filter = fil = random.choice(try_filters)
   return fil(letter.lower())
-  
+
 def random_style_text(text):
 	letters = list(text)
 	return " ".join(map(random_style_letter, letters))
 
-print(random_style_text("Hello there!"))
-
-
-bot = telebot.TeleBot(os.environ("BOT_TOKEN"));
+bot = telebot.TeleBot(os.environ["BOT_TOKEN"])
 
 @bot.message_handler(commands=['start', 'help'])
 def on_start(message):
-  bot.reply_to(message, "Привет, я случайным способом меняю текст с такого вот на такой")
+  bot.send_message(message.chat.id, "Привет. Я случайным способом меняю текст с такого вот на " + random_style_text("такой"), parse_mode="HTML")
 
 @bot.message_handler(content_types=['text'])
 def on_text(message):
-  bot.reply_to(message, "Привет, я случайным способом меняю текст с такого вот на такой")
+  bot.send_message(message.chat.id, random_style_text(message.text), parse_mode="HTML")
 
 @bot.inline_handler(lambda query: True)
 def on_inline(inline_query):
-  styled_text = random_style_text(text)
-  r = types.InlineQueryResultArticle(styled_text, styled_text, types.InputTextMessageContent(styled_text))
+  if len(inline_query.query) < 1:
+    return
+  answers = list()
+  for i in range(10):
+    styled_text = random_style_text(inline_query.query)
+    ans_id = hashlib.md5(styled_text.encode("utf-8")).hexdigest()
+    answers.append(types.InlineQueryResultArticle(ans_id, styled_text, types.InputTextMessageContent(styled_text, parse_mode="HTML")))
+  bot.answer_inline_query(inline_query.id, answers)
 
-bot.polling(none_stop=True, interval=9999)
-
+bot.remove_webhook()
+bot.polling(none_stop=True, interval=0, timeout=9999)
